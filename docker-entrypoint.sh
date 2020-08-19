317e23a49509bef4633760cd759cdc2cbdc9fbcf
Aaron <aaron.manne@semanticbits.com>
2020-08-18 17:10:45 -0700

    updates

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 set -e
 source credentials
-sed -i -e '/^Allow /s/^/#/' \
-    -e '/^ConnectPort /s/^/#/' \
-    -e '/^#DisableViaHeader /s/^#//' \
-    /etc/tinyproxy/tinyproxy.conf
-echo -e "BasicAuth $USERNAME $PASSWORD" >> /etc/tinyproxy/tinyproxy.conf
+cat /etc/tinyproxy/tinyproxy.conf
 service tinyproxy restart
 CODE=$(oathtool -b --totp "$TOTP") && echo -e "$PASSWORD\n$CODE\n" | openconnect -q -b -u "$USERNAME" --passwd-on-stdin --pfs $VPNSERVER &
 tail -f /var/log/tinyproxy/tinyproxy.log
-
