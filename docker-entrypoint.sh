3b6164d473eac5cf72fb8c30bb46a045abb217af
Aaron Manne <aaron.manne@semanticbits.com>
2020-08-20 12:31:37 -0700

    fixed auth
    tinyproxy supplied with 18.04 did not support basic auth. updated to 20.04

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 source credentials
-cat /etc/tinyproxy/tinyproxy.conf
+echo "BasicAuth $USERNAME $PASSWORD" >> /etc/tinyproxy/tinyproxy.conf
 service tinyproxy restart
 CODE=$(oathtool -b --totp "$TOTP") && echo -e "$PASSWORD\n$CODE\n" | openconnect -q -b -u "$USERNAME" --passwd-on-stdin --pfs $VPNSERVER &
 tail -f /var/log/tinyproxy/tinyproxy.log
