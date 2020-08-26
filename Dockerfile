70a9d6b76c1b8fbafa8cc23a301e4d8a0f8e1f17
Aaron Manne <aaron.manne@semanticbits.com>
2020-08-26 14:24:49 -0700

    timeout fix

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,7 @@ COPY * ./
 RUN chmod 777 docker-entrypoint.sh
 RUN apt-get update
 RUN apt-get upgrade -y
-RUN apt-get install -y openconnect tinyproxy oathtool bash dos2unix net-tools
+RUN apt-get install -y openconnect tinyproxy oathtool bash dos2unix net-tools dnsutils curl
 # dos2unix all the local files (for windows users)
 RUN find . -type f -print0 | xargs -0 dos2unix
 RUN cp tinyproxy.conf /etc/tinyproxy/tinyproxy.conf
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -e
 source credentials
-echo "BasicAuth $USERNAME $PASSWORD" >> /etc/tinyproxy/tinyproxy.conf
+#echo "BasicAuth $USERNAME $PASSWORD" >> /etc/tinyproxy/tinyproxy.conf
 service tinyproxy restart
 CODE=$(oathtool -b --totp "$TOTP") && echo -e "$PASSWORD\n$CODE\n" | openconnect -q -b -u "$USERNAME" --passwd-on-stdin --pfs $VPNSERVER &
+while true; do dig +short myip.opendns.com @resolver1.opendns.com; sleep 60; done &
 tail -f /var/log/tinyproxy/tinyproxy.log
