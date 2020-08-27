a0c231adda20f3c01bb6d232e821e7431c6f60ae
Aaron Manne <aaron.manne@semanticbits.com>
2020-08-27 11:40:07 -0700

    restart

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,6 +3,6 @@ set -e
 source credentials
 #echo "BasicAuth $USERNAME $PASSWORD" >> /etc/tinyproxy/tinyproxy.conf
 service tinyproxy restart
-CODE=$(oathtool -b --totp "$TOTP") && echo -e "$PASSWORD\n$CODE\n" | openconnect --reconnect-timeout 300 -q -b -u "$USERNAME" --passwd-on-stdin --pfs $VPNSERVER &
 while true; do dig -4 TXT +short o-o.myaddr.l.google.com @ns1.google.com; sleep 300; done &
-tail -f /var/log/tinyproxy/tinyproxy.log
+tail -f /var/log/tinyproxy/tinyproxy.log &
+CODE=$(oathtool -b --totp "$TOTP") && echo -e "$PASSWORD\n$CODE\n" | openconnect --reconnect-timeout 300 -u "$USERNAME" --passwd-on-stdin --pfs $VPNSERVER 
